fb20346b72fde0dbc40cb2152014734fc2f1f395
Jorge <jstorrespin@gmail.com>
2022-06-17 15:29:57 +0200

    adding Messages routes

--- backend/src/routes/messages.js
+++ backend/src/routes/messages.js
@@ -8,3 +8,46 @@ const Message = require('../models/message')
 router.get('/', async (req, res) => {
   res.send(await Message.find({}))
 })
+router.get('/init', async (req, res) => {
+  //
+  //    Let's delete any records that might be there first.
+  //
+  await Message.deleteMany({})
+
+  const message1 = await Message.create({
+    message: 'Hello',
+    subject: 'Titel',
+    sender: 'email ',
+    recipent: 'email 1 ',
+  })
+  console.log(message1)
+  message1.subject = 'Anything'
+  await message1.save()
+  console.log(message1)
+
+  const message2 = await Message.create({
+    message: 'Hello',
+    subject: 'Titel',
+    sender: 'email ',
+    recipent: 'email 1 ',
+  })
+  console.log(message2)
+  message2.subject = 'Something else'
+  await message2.save()
+  console.log(message2)
+
+  res.sendStatus(200)
+})
+
+router.get('/id/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const user = await Message.find({ _id: id })
+    res.send(user)
+  } catch (e) {
+    res.send(`Error: ${e.message}`)
+  }
+})
+
+module.exports = router
